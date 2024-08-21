a0fc4050933361068d9ac623a3fe5304bf677e77
saqqqib123 <mudzaf7@gmail.com>
2024-08-21 21:20:42 +0200

    Initial commit

---
+++ config-server/src/main/java/com/example/facebookclone/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.example.facebookclone;
+
+/**
+ * Hello world!
+ *
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableConfigurationProperties
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
---
+++ post-service/src/main/java/com/example/facebookclone/PostService.java
@@ -0,0 +1,15 @@
+package com.example.facebookclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class PostService {
+    public static void main(String[] args) {
+        SpringApplication.run(PostService.class, args);
+    }
+}
